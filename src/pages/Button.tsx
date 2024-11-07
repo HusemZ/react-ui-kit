6915393b2e51735f8bcca9d5d40cf01ce1297784
Hüsem K <moonrise.jpg@gmail.com>
2024-11-07 17:06:07 +0300

    feat: button component made

---
+++ src/pages/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtonPage = () => {
+  return (
+    <div>
+      <h1>Button Page</h1>
+      <button>Button</button>
+    </div>
+  )
+}
+
+export default ButtonPage
